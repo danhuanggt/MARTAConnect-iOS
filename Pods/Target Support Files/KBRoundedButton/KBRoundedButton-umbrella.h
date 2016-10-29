6a280d71d1285239506888caa3df0b064321acc6
Dan Huang <dhuang@Dans-MacBook-Pro.local>
2016-10-29 03:00:22 -0400

    Pod Install

---
+++ Pods/Target Support Files/KBRoundedButton/KBRoundedButton-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "KBRoundedButton.h"
+#import "UIImage+Color.h"
+
+FOUNDATION_EXPORT double KBRoundedButtonVersionNumber;
+FOUNDATION_EXPORT const unsigned char KBRoundedButtonVersionString[];
+
