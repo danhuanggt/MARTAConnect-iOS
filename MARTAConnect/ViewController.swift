6c71fdac5fc5a2e8e8fb8aec43b5d5652267f0fc
Dan Huang <dhuang@Dans-MacBook-Pro.local>
2016-10-29 02:42:41 -0400

    Initial Commit

---
+++ MARTAConnect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MARTAConnect
+//
+//  Created by Dan Huang on 10/29/16.
+//  Copyright © 2016 Technic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
