5dc004e39954809f242e0cc12c262b27c8d30949
Dan Huang <dhuang@Dans-MacBook-Pro.local>
2016-10-29 04:35:46 -0400

    Location permission handling

--- MARTAConnect/InitialViewController.swift
+++ MARTAConnect/InitialViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class InitialViewController: UIViewController {
+class InitialViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup Location
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,6 +54,47 @@ class InitialViewController: UIViewController {
                 self.present(networkErrorAlertController, animated: true, completion: nil)
             }
         }
+    }
+    
+    // MARK: CoreLocation
+    
+    func checkLocationPermission() {
+        let locationAuthorizationStatus = CLLocationManager.authorizationStatus()
+        switch locationAuthorizationStatus {
+        case .notDetermined:
+            // Request permission
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted, .denied:
+            // Display permission alert
+            let locationPermissionErrorTitle = "Location Permission Error"
+            let locationPermissionErrorMessage = "MARTAConnect needs permission to access your location.\n\nGo to your iOS Settings → Privacy → Location Services and enable access for MARTAConnect."
+            
+            let alertController = UIAlertController(title: locationPermissionErrorTitle,
+                                                    message: locationPermissionErrorMessage,
+                                                    preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok",
+                                         style: UIAlertActionStyle.default)
+            { (action: UIAlertAction) -> Void in
+                // Do Nothing
+            }
+            
+            alertController.addAction(okAction)
+            DispatchQueue.main.async(execute: {
+                self.present(alertController, animated: true, completion: nil)
+            })
+            break
+        default:
+            // Start getting location
+            locationManager.startUpdatingLocation()
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // Check permission for location services
+        checkLocationPermission()
     }
 }
 
